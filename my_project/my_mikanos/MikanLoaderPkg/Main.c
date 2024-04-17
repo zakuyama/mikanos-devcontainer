c11738e926e51b1b7096b6aa3c7675d65f5102ca
zakuyama <zakuyama@users.noreply.github.com>
2024-04-17 11:23:22 +0900

    Add MemoryMap

--- my_project/my_mikanos/MikanLoaderPkg/Main.c
+++ my_project/my_mikanos/MikanLoaderPkg/Main.c
@@ -1,5 +1,40 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/PrintLib.h>
+#include <Protocol/LoadedImage.h>
+#include <Protocol/SimpleFileSystem.h>
+#include <Protocol/DiskTo2.h>
+#include <Protocol/BlockIo.h>
+
+// メモリマップは、メモリ領域に書き込まれるデータの構造(p.57)
+// 構造体で表す
+struct MemoryMap{
+    UINTN buffer_size;
+    VOID* buffer;
+    UINTN map_size;
+    UINTN map_key;
+    UINTN descriptor_size;
+    UINT32 descriptor_version;
+}
+
+// gXX はグローバル変数
+//
+EFI_STATUS GetMemoryMap(struct MemoryMap* map){
+    if(map->buffer == NULL){
+        return EFI_BUFFER_TOO_SMALL;
+    }
+
+    map->map_size = map->buffer_size;
+
+    return gBS->GetMemoryMap(
+        &map->map_size,
+        (EFI_MEMORY_DESCRIPTOR*)map->buffer,
+        &map->map_key,
+        &map->descriptor_size,
+        &map->descriptor_version);
+    )
+}
 
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
